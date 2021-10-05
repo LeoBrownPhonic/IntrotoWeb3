4398ed0b5de8334180a2b56fb103f935aa133bd5
Anamaria-Mocanu <Annamaariaaa@yahoo.com>
2021-10-05 15:55:32 +0100

    Introducing params completed

--- app.rb
+++ app.rb
@@ -9,6 +9,13 @@ get "/secret" do
   "SEC"
 end
 
-get "/cat" do
+get "/random-cat" do
+  @name_generator = ["Amigo", "Misty", "Almond"].sample
+  erb(:index)
+end
+
+get "/named-cat" do
+  p params
+  @name_generator = params[:name]
   erb(:index)
 end
